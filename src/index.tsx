5c3aa57d3600758fdd0c8127efe8fb9619a61197
Makoto <makoto@emurasoft.com>
2021-12-08 19:17:35 -0800

    uci example

--- src/index.tsx
+++ src/index.tsx
@@ -6,3 +6,8 @@ const stockfish = new Worker('stockfish.js');
 stockfish.onmessage = function(event) {
 	console.log(event);
 }
+
+stockfish.postMessage("uci");
+stockfish.postMessage("setoption name Threads value 4");
+stockfish.postMessage("position fen 3rb1k1/1Bp2pp1/4p3/2P1P2p/r5nP/1N4P1/P4P2/R3R1K1 b - - 0 27");
+stockfish.postMessage("go depth 20");
