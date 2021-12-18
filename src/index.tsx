b6a9ffdc9c885917a292ebd86ebe0bdf95319fad
Makoto <makoto@emurasoft.com>
2021-12-17 17:22:51 -0800

    router

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import {App} from './App';
+import {App} from './components/App';
 import {Provider} from 'react-redux';
 import {store} from './store';
 
