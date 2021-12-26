127ac1857041ec484b1f203130b00e9a9630e2ad
Makoto <makoto@emurasoft.com>
2021-12-26 09:25:26 -0800

    remove react-router

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Game from './components/Game';
 import { store } from './store';
 import ErrorBoundary from './components/ErrorBoundary';
@@ -10,12 +9,7 @@ import ErrorBoundary from './components/ErrorBoundary';
 ReactDOM.render(
   <ErrorBoundary>
     <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Game />} />
-          <Route path="*" element={<p>Page not found</p>} />
-        </Routes>
-      </BrowserRouter>
+      <Game />
     </Provider>
   </ErrorBoundary>,
   document.getElementById('root'),
