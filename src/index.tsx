b614c444b1f35d1373f4c4559951a51e0955ac01
Makoto <makoto@emurasoft.com>
2022-01-02 20:47:54 -0800

    rename

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import { Provider } from 'react-redux';
-import Game from './components/Game';
+import App from './components/App';
 import { store } from './store';
 import ErrorBoundary from './components/ErrorBoundary';
 
 ReactDOM.render(
   <ErrorBoundary>
     <Provider store={store}>
-      <Game />
+      <App />
     </Provider>
   </ErrorBoundary>,
   document.getElementById('root'),
