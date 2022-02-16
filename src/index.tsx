73c09578f12b6ed89b86518c4340d5a41f636ecb
Makoto <makoto@emurasoft.com>
2022-02-15 19:15:56 -0800

    Revert "routes"
    This reverts commit 751f691c6815914efdf372d5c308875d0b0836fa.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './components/App';
 import ErrorBoundary from './components/ErrorBoundary';
 // eslint-disable-next-line import/no-relative-packages
@@ -8,11 +7,7 @@ import '../semantic/semantic.less';
 
 ReactDOM.render(
   <ErrorBoundary>
-    <BrowserRouter basename="/guess-the-eval">
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </ErrorBoundary>,
   document.getElementById('root'),
 );
